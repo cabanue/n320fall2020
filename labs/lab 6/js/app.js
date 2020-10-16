52c893926975ffd4c9586f94bccf4b777cd910c9
cabanue <cabanue@iu.edu>
2020-10-16 17:10:38 -0400

    Comments
    added comments to app.js

--- labs/lab 6/js/app.js
+++ labs/lab 6/js/app.js
@@ -1,3 +1,5 @@
+//Sets up component using vue to display all the books. 
+//Allows for multiple book entries as it has the template
 Vue.component("books-view",{
     props: ["book"],
     template: `<div id='bookStyle' v-if='book.visibility'> 
@@ -11,12 +13,16 @@ let app = new Vue({
     el: "#app",
     data: {
         header: "Book List",
+        //array of objects of books
         books: [
             {id: 0, title: "The Throne of Fire", emoji: "🔥", author: "Rick Riordan", visibility: true},
             {id: 1, title: "The Serpent's Shadow", emoji: "🐍", author: "Rick Riordan", visibility: true}
         ]
     },
     methods: {
+        //function to change visibility of the second book. 
+        //checks if value is true or not. If it is currently true, switches to false, hiding the second book.
+        //if it is currently false, switches it to true, making the second book visible.  
         changeVisible: function() {
             if (this.books[1].visibility == true){
                 this.books[1].visibility = false
