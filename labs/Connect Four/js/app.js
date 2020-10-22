cb20076cb5d8856aced2f20cbc2455d7839ce3cb
cabanue <cabanue@iu.edu>
2020-10-22 17:35:42 -0400

    Connect Four
    added link to homepage, pasted code from board game deconstruction assignment, started work on js

---
+++ labs/Connect Four/js/app.js
@@ -0,0 +1,56 @@
+//uncommented code is current interactivity of svg
+//changing colors of circles will be used later for indicating who placed 
+let circles = document.getElementsByClassName("b")
+
+for(i=0; i < circles.length; i++){
+    circles[i].addEventListener("click", placePiece);
+}
+
+let spaces = circles;
+
+function placePiece(event){
+    event.target.style.fill = "#ad1403";
+}
+
+//#ad1403 color for player two: red
+//#f7df2a color for player two: yellow
+
+//will have properties for the spaces(will store circles in array)
+class Grid {
+    constructor(spaces){
+        this.spaces = circles;
+    }
+
+
+    //checks for win condition 
+    win(){
+
+    }
+
+    //will clear the spaces to be able to play again 
+    clear(){
+        for(i=0; i < this.spaces.length; i++){
+            spaces[i].style.fill = "white";
+        }
+    }
+}
+//will store who the players are, names, and color of piece they chose 
+class Piece {
+    
+    //placing the piece on the board
+    place(){
+
+    }
+
+    //resets to have new players and change colors for pieces 
+    reset(){
+
+    }
+}
+
+//store 2 players in an array 
+
+//will have properties of the name of the player, color they choose for the piece, and will display who wins in top DIV 
+class Player {
+
+}
